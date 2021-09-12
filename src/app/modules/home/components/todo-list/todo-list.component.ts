30395fa2fd66ec785ffe364c726060a591778a86
Lucas Henrick <lucashenrikc@gmail.com>
2021-09-12 13:10:46 -0300

    Adicionando Local Storage.

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,5 +1,5 @@
 import { TaskList } from './../../../model/task-list';
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { ThrowStmt } from '@angular/compiler';
 
 @Component({
@@ -7,13 +7,16 @@ import { ThrowStmt } from '@angular/compiler';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements OnInit,DoCheck {
 
-  public taskList: Array<TaskList> = [];
-
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem('list') || '[]');
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngDoCheck():void{
+    this.setLocalStorate()
   }
 
   public deleteItemTaskList(event: number){
@@ -27,6 +30,11 @@ export class TodoListComponent implements OnInit {
   public setEmitTaskList(event: string){
     this.taskList.push({task: event, checked: false});
   }
-
+  public setLocalStorate(){
+    if(this.taskList){
+      this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked))
+      localStorage.setItem('list', JSON.stringify(this.taskList));
+    }
+  }
 
 }
